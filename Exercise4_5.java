ebdd1eb7bb14504a1fc00c08309ee7bb53cf714a
Şevval Yıldız <250541025@firat.edu.tr>
2025-12-03 21:24:29 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -1,27 +1,30 @@
-public class Exercise4_5 {
+Ad Soyad: Şevval Yıldız
+Öğrenci No: 250541025
+
+    public class Exercise4_5 {
 
     public static void zoop(String fred, int bob) {
-        System.out.println(fred);
-        if (bob == 5) {
-            ping("not ");
+        System.out.println(fred);                 // 3
+        if (bob == 5) {                           // 4 (true)
+            ping("not ");                         // 5
         } else {
             System.out.println("!");
         }
     }
 
     public static void main(String[] args) {
-        int bizz = 5;
-        int buzz = 2;
-        zoop("just for", bizz);
-        clink(2 * buzz);
+        int bizz = 5;                             // 1
+        int buzz = 2;                             // 1.1
+        zoop("just for", bizz);                   // 2
+        clink(2 * buzz);                          // 6
     }
 
     public static void clink(int fork) {
-        System.out.print("It's ");
-        zoop("breakfast ", fork);
+        System.out.print("It's ");                // 7
+        zoop("breakfast ", fork);                 // 8
     }
 
     public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more ");
+        System.out.println("any " + strangStrung + "more "); // 5.1 ve 9
     }
 }
