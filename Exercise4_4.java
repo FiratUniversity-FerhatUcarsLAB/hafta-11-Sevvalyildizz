f225ce791744d9a7810b1115553311adecb8f013
Şevval Yıldız <250541025@firat.edu.tr>
2025-12-03 21:23:11 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,5 @@
+Ad Soyad:Şevval Yıldız
+Öğrenci No:250541025
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -12,10 +14,13 @@ public class Exercise4_4 {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
+        // Hiçbir şey olmaz. Metot 42 değerini döndürür ama biz kullanmadığımız için
+        // ekrana herhangi bir çıktı verilmez. Sadece değer boşa gider.
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
-
-        // Cevaplarinizi yorum olarak ekleyin.
+        // Hata verir. Çünkü sayHello() void döner, yani bir değer döndürmez.
+        // Bu yüzden 7 ile toplanamaz ve derleme hatası oluşur.
     }
 }
+
