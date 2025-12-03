b5bded54fb9755d00a965840c0a0664b5d50e67f
Şevval Yıldız <250541025@firat.edu.tr>
2025-12-03 21:25:39 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,27 @@
-public class Exercise4_6_Multadd {
+Ad Soyad: Şevval Yıldız
+Öğrenci No: 250541025
+
+    public class Exercise4_6_Multadd {
 
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        // multadd(a, b, c) = a * b + c
+        return a * b + c;
     }
 
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        // expSum(x) = x * e^(-x) + sqrt(1 - e^(-x))
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        // Test 1: basit multadd
+        System.out.println(multadd(1.0, 2.0, 3.0)); 
+        // Beklenen çıktı: 5.0
+
+        // Test 2: expSum fonksiyonu
+        System.out.println(expSum(2.0));
+        // x = 2 için expSum hesaplar
     }
 }
+
